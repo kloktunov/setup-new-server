89355bcf336f1ae0340c23d19d3d26223359dac7
Alexey Kloktunov <kloktun@gmail.com>
2021-10-07 14:10:19 +0300

    Init commit

---
+++ setup.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+
+# ===============================
+# SETUP NEW USER
+# ===============================
+read -p "Введите новое имя пользователя: " username;
+read -p "Введите пароль пользователя: " password;
+adduser $username;
+usermod -aG sudo $username;
+
+
+# ===============================
+# SETUP UTILS
+# ===============================
+apt-get update;
+apt-get install -y mc;
+apt-get install -y git;
+apt-get install curl -y;
+
+
+# ==================================
+# SETUP NGINX
+# ==================================
+apt-get install nginx -y;
+
+curl https://raw.githubusercontent.com/kloktunov/nginx-configs/master/default > /etc/nginx/sites-available/default;
+
+
+sed -i "s/USERNAME/$username/" /etc/nginx/sites-available/default;
+sed -i "s/# server_names_hash_bucket_size 64/server_names_hash_bucket_size 256/" /etc/nginx/nginx.conf;
+
+service nginx restart;
+
+# ==================================
+# SETUP NODEJS
+# ==================================
+apt install ca-certificates;
+
+rm /usr/share/ca-certificates/mozilla/DST_Root_CA_X3.crt;
+update-ca-certificates;
+
+
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -;
+
+apt-get update;
+
+apt-get install nodejs -y;
+apt-get install certbot -y;
+
+npm install -g pm2;
+pm2 startup;
+
+# ==================================
+# SETUP POSTGRESQL
+# ==================================
+su - $username << EOF
+    
+    echo $password | sudo -S echo "setup password";
+    mkdir ~/download;
+    mkdir ~/cert;
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -;
+    RELEASE=$(lsb_release -cs);
+    echo "deb http://apt.postgresql.org/pub/repos/apt/ ${RELEASE}"-pgdg main | sudo tee  /etc/apt/sources.list.d/pgdg.list;
+    sudo apt update;
+    sudo apt -y install postgresql-11;
+    sudo ss -tunelp | grep 5432;
+
+EOF
+
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/11/main/postgresql.conf;
+echo 'host  all  all 0.0.0.0/0 md5' >>  /etc/postgresql/11/main/pg_hba.conf;
+
+su - postgres << EOF
+
+    # read -p "Введите пароль для базы данных: " dbpassword;
+    psql -c "alter user postgres with password '$password'";
+    
+#    read -p "Введите название базы данных: " $dbname;
+#    psql -c "CREATE DATABASE $dbname";
+    
+    echo "DB User: postgres";
+    echo "DB Password: $password";
+#    echo "DB Name: $dbname";
+
+EOF
+
+service postgresql restart;
