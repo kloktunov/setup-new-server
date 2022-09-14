315601b4a6eba64b8d6ab5b1bbdea748928cf216
Alexey Kloktunov <kloktun@gmail.com>
2022-09-14 19:00:26 +0300

    Update setup-new.sh

--- setup-new.sh
+++ setup-new.sh
@@ -64,8 +64,9 @@ apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y;
 echo "# ================================";
 echo "# INSTALL DOCKER COMPOSE";
 echo "# ================================";
-curl -L https://github.com/docker/compose/releases/download/1.25.3/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose;
-sudo chmod +x /usr/local/bin/docker-compose;
+VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
+curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose;
 docker-compose --version;
 usermod -aG docker $username;
 
